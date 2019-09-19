8764882d3f2d4a81c92e34e842a4880058755ca0
fuzzylogicxx <laurence@fuzzylogicdigital.com>
2019-09-19 17:48:35 +0100

    Refactored routes to use async/await rather than promise chaining

--- src/index.js
+++ src/index.js
@@ -10,27 +10,85 @@ const port = process.env.PORT || 3000
 // to an object so we we can use it
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+
+
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
-    user.save().then(() => {
-        res.status(201).send(user)
-    }).catch((error) => {
+    try {
+        await user.save()
+        res.status(201).send(user)    
+    } catch (error) {
         res.status(400).send(error)
-    })
+    }
 })
 
-app.post('/tasks', (req, res) => {
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+app.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const user = await User.findById(_id)
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+
+
+
+
+
+
+
+
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
-    task.save().then(() => {
+    try {
+        await task.save()
         res.status(201).send(task)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
+    } catch (e) {
+        res.status(400).send(e)    
+    }
 })
 
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send()    
+    }
+})
 
+app.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)    
+    }
+})
 
 
 
